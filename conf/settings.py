1cb174fa853960c2a0ffce8f9132e12911959001
Vanda Farsad <v.farsad@orendtstudios.com>
2023-12-03 16:40:51 +0100

    add prod dockerfile and remove docker-compose of react container

--- conf/settings.py
+++ conf/settings.py
@@ -129,10 +129,12 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "frontend/build/static",
+    # BASE_DIR / "static",
+    BASE_DIR
+    / "frontend/build/static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
